010f159a6229aab46d63a909ac3e57582731a3e3
iMillJoe <josephmillion@me.com>
2014-05-13 20:59:52 -0500

    Fixed crash after five questions. (Only had five questions, and was not checking for empty array) Questions now loop randomly.

--- Hippopotamath/IMHippoGameBrain.m
+++ Hippopotamath/IMHippoGameBrain.m
@@ -13,45 +13,9 @@
 -(id)init
 {
     self = [super init];
+    
     if (self) {
-        
-        //setup game logic
-        IMMutipalChoiceQuestion* one = [[IMMutipalChoiceQuestion alloc] init];
-        one.question = @"What is CS50";
-        one.answer = @"This! (is CS50)";
-        one.wrongAnswers = [NSMutableArray arrayWithArray: @[@"Nothing is CS50", @"Another wrong answer", @"Yet antoher wrong one"]];
-        
-        IMMutipalChoiceQuestion* two = [[IMMutipalChoiceQuestion alloc] init];
-        two.question = @"5 + 5 = ?";
-        two.answer = @"10";
-        two.wrongAnswers = [NSMutableArray arrayWithArray: @[@"9", @"13", @"7"]];
-        
-        IMMutipalChoiceQuestion* three = [[IMMutipalChoiceQuestion alloc] init];
-        three.question = @"5 - 3 = ?";
-        three.answer = @"2";
-        three.wrongAnswers = [NSMutableArray arrayWithArray: @[@"1", @"8", @"4"]];
-        
-        IMMutipalChoiceQuestion* four = [[IMMutipalChoiceQuestion alloc] init];
-        four.question = @"5 + ? = 8";
-        four.answer = @"3";
-        four.wrongAnswers = [NSMutableArray arrayWithArray: @[@"5", @"2", @"7"]];
-        
-        IMMutipalChoiceQuestion* five = [[IMMutipalChoiceQuestion alloc] init];
-        five.question = @"? + 3 = 5";
-        five.answer = @"2";
-        five.wrongAnswers = [NSMutableArray arrayWithArray: @[@"7", @"1", @"3"]];
-        
-        //IMMutipalChoiceQuestion* six = [[IMMutipalChoiceQuestion alloc] init];
-        //IMMutipalChoiceQuestion* seven = [[IMMutipalChoiceQuestion alloc] init];
-        //IMMutipalChoiceQuestion* eight = [[IMMutipalChoiceQuestion alloc] init];
-        //IMMutipalChoiceQuestion* nine = [[IMMutipalChoiceQuestion alloc] init];
-        //IMMutipalChoiceQuestion* ten = [[IMMutipalChoiceQuestion alloc] init];
-        
-        self.questionsArray = [NSMutableArray arrayWithArray: @[one, two, three, four, five,]];
-        
-        
-        
-        
+        [self setupGame];
         return self;
     }
     
@@ -60,7 +24,11 @@
 
 
 -(IMMutipalChoiceQuestion *) pickRandomQuestion
-{    
+{
+    if (![self.questionsArray count] == 0) {
+        [self setupGame];
+    }
+    
     int questionIndex = arc4random() % [self.questionsArray count];
     
     IMMutipalChoiceQuestion* thisQuestion = [self.questionsArray objectAtIndex: questionIndex];
@@ -69,6 +37,41 @@
     return thisQuestion;
 }
 
-
+- (void) setupGame
+{
+    //setup game logic
+    IMMutipalChoiceQuestion* one = [[IMMutipalChoiceQuestion alloc] init];
+    one.question = @"What is CS50";
+    one.answer = @"This! (is CS50)";
+    one.wrongAnswers = [NSMutableArray arrayWithArray: @[@"Nothing is CS50", @"Another wrong answer", @"Yet antoher wrong one"]];
+    
+    IMMutipalChoiceQuestion* two = [[IMMutipalChoiceQuestion alloc] init];
+    two.question = @"5 + 5 = ?";
+    two.answer = @"10";
+    two.wrongAnswers = [NSMutableArray arrayWithArray: @[@"9", @"13", @"7"]];
+    
+    IMMutipalChoiceQuestion* three = [[IMMutipalChoiceQuestion alloc] init];
+    three.question = @"5 - 3 = ?";
+    three.answer = @"2";
+    three.wrongAnswers = [NSMutableArray arrayWithArray: @[@"1", @"8", @"4"]];
+    
+    IMMutipalChoiceQuestion* four = [[IMMutipalChoiceQuestion alloc] init];
+    four.question = @"5 + ? = 8";
+    four.answer = @"3";
+    four.wrongAnswers = [NSMutableArray arrayWithArray: @[@"5", @"2", @"7"]];
+    
+    IMMutipalChoiceQuestion* five = [[IMMutipalChoiceQuestion alloc] init];
+    five.question = @"? + 3 = 5";
+    five.answer = @"2";
+    five.wrongAnswers = [NSMutableArray arrayWithArray: @[@"7", @"1", @"3"]];
+    
+    //IMMutipalChoiceQuestion* six = [[IMMutipalChoiceQuestion alloc] init];
+    //IMMutipalChoiceQuestion* seven = [[IMMutipalChoiceQuestion alloc] init];
+    //IMMutipalChoiceQuestion* eight = [[IMMutipalChoiceQuestion alloc] init];
+    //IMMutipalChoiceQuestion* nine = [[IMMutipalChoiceQuestion alloc] init];
+    //IMMutipalChoiceQuestion* ten = [[IMMutipalChoiceQuestion alloc] init];
+    
+    self.questionsArray = [NSMutableArray arrayWithArray: @[one, two, three, four, five,]];
+}
 
 @end
